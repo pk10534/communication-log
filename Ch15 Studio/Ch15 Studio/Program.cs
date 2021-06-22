713d9088332283df6b277de9547f8d33c785d242
steven.j.wilcox@gmail.com <steven.j.wilcox@gmail.com>
2021-06-22 15:01:32 -0400

    new color and joke

--- Ch15 Studio/Ch15 Studio/Program.cs
+++ Ch15 Studio/Ch15 Studio/Program.cs
@@ -9,6 +9,8 @@ namespace Ch15_Studio
             Console.WriteLine("Hello Pilot!");
             Console.WriteLine("Help me Control!!!");
             Console.WriteLine("How can we be of assistance, Pilot?");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Autocorrect can go straight to he’ll");
         }
     }
 }
